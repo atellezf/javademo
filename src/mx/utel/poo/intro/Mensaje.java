3f1f73d7eda53c09cd23300691ba5e96ed172f02
atellezf <alejandro.tellez@gmail.com>
2021-01-08 21:51:03 -0600

    Código de OC-S1

---
+++ src/mx/utel/poo/intro/Mensaje.java
@@ -0,0 +1,9 @@
+package mx.utel.poo.intro;
+
+public class Mensaje {
+	
+	public void imprimir(String nombre) {
+		System.out.printf("Hola %s\n", nombre);
+	}
+	
+}
---
+++ src/mx/utel/poo/intro/Programa.java
@@ -0,0 +1,16 @@
+package mx.utel.poo.intro;
+
+import java.util.Scanner;
+
+public class Programa {
+	
+	public static void main(String[] args) {		
+		Mensaje m = new Mensaje();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Escribe tu nombre: ");
+		String nombre = scan.nextLine();
+		m.imprimir(nombre);
+	}
+	
+}
+
